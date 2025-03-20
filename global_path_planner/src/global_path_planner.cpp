1a5ff181a616ea47e7e7f7d3b0a5b72e1a536b60
Toshiki-Takesue <ee223135@meiji.ac.jp>
2025-03-20 12:43:28 +0900

    second commit

--- global_path_planner/src/global_path_planner.cpp
+++ global_path_planner/src/global_path_planner.cpp
@@ -4,7 +4,7 @@ using namespace std::chrono_literals;
 
 // デフォルトコンストラクタ
 // パラメータの宣言と取得
-Astar::Astar() : Node("team_path_planner"), clock_(RCL_ROS_TIME)
+Astar::Astar() : Node("teamb_path_planner"), clock_(RCL_ROS_TIME)
 {
     // ###### パラメータの宣言 ######
 
@@ -20,18 +20,26 @@ Astar::Astar() : Node("team_path_planner"), clock_(RCL_ROS_TIME)
 
 
     // ####### Subscriber #######
-
+    sub_map_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>("/map",rclcpp::QoS(1).reliable(),std::bind(&Astar::map_callback,this,std::placeholders::_1));
 
     // ###### Publisher ######
-    
+    pub_path_ = this->create_publisher<nav_msgs::msg::Path>("/g_path",rclcpp::QoS(1).reliable());
+    pub_node_point_ = this->create_publisher<geometry_msgs::msg::PointStamped>("/pub_node_point",rclcpp::QoS(1).reliable());
+    pub_current_path_ = this->create_publisher<nav_msgs::msg::Path>("/g_path",rclcpp::QoS(1).reliable());
+    pub_new_map_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/map_new",rclcpp::QoS(1).reliable());
 }
 
 // mapのコールバック関数
 // msgを受け取り，map_に代入，その情報をそれぞれ取得
 // process()を実行
-void Astar::map_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg)  //マップの読み込み
+void Astar::map_callback(const nav_msgs::msg::OccupancyGrid msg)  //マップの読み込み
 {
+    map_ = *msg;
+    //https://docs.ros.org/en/lunar/api/nav_msgs/html/msg/OccupancyGrid.html
+    //ChatGPTでは-1(未知)、0(空き)、100(障害物)を示す
 
+    resolution_ = msg -> info.resolution;//単位格子当たりの大きさを格納
+    RCLCPP_INFO(this->get_logger(), "Grid size: %f meters per cell", resolution_);
 }
 
 // マップ全体の障害物を拡張処理（new_map_をpublishする）
@@ -50,11 +58,27 @@ void Astar::obs_expand(const int index)
 double Astar::make_heuristic(const Node_ node)
 {
 
+    int vertical = 0;//縦マス
+    int side = 0;//横マス
+    double heuristic = 0.0;//ヒューリスティック関数
+    vertical = abs(node.y -goal_node_.y);//目標とゴールまでの差
+    side = abs(node.x - goal_node_.x); //目標とゴールまでの差
+    return heuristic = sqrt(vertical * vertical + side *side);
+    //ピタゴラスの定理を用いて障害物除いた距離を算出
+
 }
 
 // スタートとゴールの取得（mからグリッド単位への変換も行う）
 Node_ Astar::set_way_point(int phase)
 {
+    if(phase == 0){//スタート地点の格納にはphase=0
+        start_node_.x = node.x;//スタート地点のｘ座標を格納
+        start_node_.y = node.y;//スタート地点のｙ座標を格納
+    }
+    if(phase == 1){//ゴール地点の格納にはphase=1
+        goal_node_.x = node.x;//目標のｘ座標を格納
+        goal_node_.y = node.y;//目標のｙ座標を格納
+    }
 
 }
 
@@ -82,44 +106,65 @@ geometry_msgs::msg::PoseStamped Astar::node_to_pose(const Node_ node)
 // openリスト内で最もf値が小さいノードを取得する関数
 Node_ Astar::select_min_f()
 {
-
+    double smallest_f = 0.0;//整数でも対応できるように変数設定　autoは？
+    smallest_f = std::min_element(open_list_.begin(),open_list_.end(), [](const Node_& a, const Node_& b){
+        return a.f < b.f;
+    });//min_elementは比較によって最小値を見つけ出す関数
 }
 
 // スタートノードの場合，trueを返す
 bool Astar::check_start(const Node_ node)
 {
-
+    return node = start_node_;
 }
 
 // ゴールノードの場合，trueを返す
 bool Astar::check_goal(const Node_ node)
 {
-
+    return node = goal_node_;
 }
 
 // 2つが同じノードである場合，trueを返す
 bool Astar::check_same_node(const Node_ n1, const Node_ n2)
 {
+    // if(n1.x == n2.x && n1.y == n2.y){
+    //     return true;//x座標とy座標が同じであればtrue
+    // }else{
+    //     return false;
+    // }
 
+    return n1 = n2;
 }
 
 // 指定したリストに指定のノードが含まれるか検索
 //（含まれる場合はインデックス番号を返し，含まれない場合は-1を返す）
 int Astar::check_list(const Node_ target_node, std::vector<Node_>& set)
 {
+    auto result = std::find_if(list.begin(),list.end(), [](const Node_& n){
+        return check_same_node(n,node);//同じノードを探す
+        //aytoで大丈夫？それからインデックス番号って？
+    });
 
+    if(result == list.end()){
+        return -1;
+    }
+    //下にも同じような関数が一つあるけどどう違うの？
 }
 
 // list1から指定されたノードを探し，リスト1から削除してリスト2に移動する関数
 void Astar::swap_node(const Node_ node, std::vector<Node_>& list1, std::vector<Node_>& list2)
 {
+    auto result = search_node_from_list(node,list1);//autoだけ心配
+
+    list2.push_back(*result);//見つけたノードをlist2の末尾に追加
+    list1.erase(result);//list2から指定のノードを削除   
 
 }
 
 // 指定のノードが障害物である場合，trueを返す
 bool Astar::check_obs(const Node_ node)
 {
-
+    return  >= 50;//mapデータから障害物であるかどうかの判定をするが。果たしてNodeの情報を使用するのか
 }
 
 // 隣接ノードを基にOpenリスト・Closeリストを更新
@@ -151,15 +196,28 @@ void Astar::create_neighbor_nodes(const Node_ node, std::vector<Node_>&  neighbo
 // 動作モデルを作成（前後左右，斜めの8方向）
 void Astar::get_motion(std::vector<Motion_>& list)
 {
-    list.push_back(motion( 1, 0, 1)); // 前
-    // ###### 上を参考に動作モデルの追加 ######
-
+    list.push_back(motion( 1, 0, 1.0));// 前
+    list.push_back(motion(-1, 0, 1.0));// 後
+    list.push_back(motion( 0,-1, 1.0));// 左 
+    list.push_back(motion( 0, 1, 1.0));// 右
+    list.push_back(motion( 1, 1, 1.414));// 右前
+    list.push_back(motion(-1, 1, 1.414));// 右後
+    list.push_back(motion( 1,-1, 1.414));// 左前
+    list.push_back(motion(-1,-1, 1.414));// 左後
 }
 
 // 与えられたdx, dy, costを基にモーション（移動）を作成
 // 隣接したグリッドに移動しない場合はエラーメッセージを出力して終了
 Motion_ Astar::motion(const int dx,const int dy,const int cost)
 {
+    dx = abs(goal_node_.x - start_node_.y);//スタートからゴールまでのx座標移動距離
+    dy = abs(goal_node_.y - start_node_.y);//スタートからゴールまでのy座標移動距離
+
+    //斜め移動を考慮しなくてよいのか
+    // int difference = 0;//差分を設定
+    // difference = abs(dx - dy);//差を求めることによって斜め移動ができるか確認
+    
+    cost = dx + dy;//単純に加算してコストを求める
 
 }
 
@@ -191,7 +249,13 @@ bool Astar::check_parent(const int index, const Node_ node)
 // 見つからなければ-1を返す
 int Astar::search_node_from_list(const Node_ node, std::vector<Node_>& list)
 {
+    auto result = std::find_if(list.begin(),list.end(), [](const Node_& n){
+        return check_same_node(n,node);//同じノードを探す　autoだけ心配
+    });
 
+    if(result == list.end()){
+        return -1;
+    }
 }
 
 
